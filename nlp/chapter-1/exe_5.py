e26e022c7b2e3796468ef2c99bc43af9c7cda1c2
kirankumarcs02 <kirankumarcs02@gmail.com>
2019-08-14 23:19:50 +0530

    chapter-1

---
+++ nlp/chapter-1/exe_5.py
@@ -0,0 +1,41 @@
+# Import the necessary modules
+from nltk.tokenize import regexp_tokenize
+from nltk.tokenize import TweetTokenizer
+
+tweets = ['This is the best #nlp exercise ive found online! #python',
+ '#NLP is super fun! <3 #learning',
+ 'Thanks @datacamp :) #nlp #python']
+
+
+# Define a regex pattern to find hashtags: pattern1
+pattern1 = r"#\w+"
+
+# Use the pattern on the first tweet in the tweets list
+regexp_tokenize(tweets[0], pattern1)
+
+# Write a pattern that matches both mentions and hashtags
+pattern2 = r"([@#]\w+)"
+
+# Use the pattern on the last tweet in the tweets list
+regexp_tokenize(tweets[-1], pattern2)
+
+# Use the TweetTokenizer to tokenize all tweets into one list
+tknzr = TweetTokenizer()
+all_tokens = [tknzr.tokenize(t) for t in tweets]
+print(all_tokens)
+
+
+german_text='Wann gehen wir Pizza essen? 🍕 Und fährst du mit Über? 🚕'
+
+
+# Tokenize and print all words in german_text
+all_words = word_tokenize(german_text)
+print(all_words)
+
+# Tokenize and print only capital words
+capital_words = r"[A-Z|Ü]\w+"
+print(regexp_tokenize(german_text, capital_words))
+
+# Tokenize and print only emoji
+emoji = "['\U0001F300-\U0001F5FF'|'\U0001F600-\U0001F64F'|'\U0001F680-\U0001F6FF'|'\u2600-\u26FF\u2700-\u27BF']"
+print(regexp_tokenize(german_text, emoji))
